257f4723432c8eb31699ef4c9b433b7fa06e88d1
Craftingmod <alyac1234@gmail.com>
2019-02-21 17:40:56 +0900

    Export module to use

--- src/launch.ts
+++ src/launch.ts
@@ -1,4 +1,38 @@
-import { one } from "./index"
+import path from "path"
+import { Ncc } from "./index"
+import Log from "./log"
 
-console.log("Hello, World!")
-console.log("One: " + one)
+Log.enable = true
+async function run() {
+    const ncc = new Ncc()
+    // ncc.autoConnect = false
+    // set cookie path to load & save
+    const cookie = path.resolve(process.cwd(), "choco.cookie")
+    ncc.cookiePath = cookie
+    // load credit
+    let uname = await ncc.loadCredit()
+    if (uname == null) {
+        // or.. otp login
+        // otp is recommend due to naver's hardest captacha
+        const otpcode = await Log.read("OTP")
+        uname = await ncc.loginOTP(otpcode)
+    }
+    if (uname == null) {
+        return
+    }
+    // gotcha!
+    Log.d(`username: ${uname}`)
+    // connect to Ncc for parse joinedchannels
+    await ncc.connect()
+    // find open Channel
+    const openCh = ncc.joinedChannels.find(
+        (v) => v.cafe.cafeId === 26686242 && v.channelInfo.name === "비공개 채팅방")
+    if (openCh == null) {
+        return
+    }
+    // session connect
+    const session = await ncc.getConnectedChannel(openCh)
+    // send chat
+    await session.sendText("안녕, 네이버!")
+}
+run()
---
+++ src/ncc/nccexport.ts
@@ -0,0 +1,49 @@
+/**
+ * It's too far to remove
+ * `export default` so copy-paste
+ */
+/* tslint:disable */
+// import ArticleParser from "./articleparser"
+import Ncc from "./ncc"
+import NCaptcha from "./ncaptcha"
+import NcFetch from "./ncfetch"
+import NcCredent from "./ncredent"
+export * from "./ncredent"
+export * from "./ncfetch"
+export * from "./ncaptcha"
+export * from "./articleparser"
+export { Ncc, NCaptcha, NcFetch, NcCredent }
+
+import NCredit from "./credit/ncredit"
+export * from "./credit/ncredit"
+export { NCredit }
+
+import Article from "./structure/article"
+import Cafe from "./structure/cafe"
+import Comment from "./structure/comment"
+import NaverVideo from "./structure/navervideo"
+import Profile from "./structure/profile"
+export * from "./structure/article"
+export * from "./structure/cafe"
+export * from "./structure/comment"
+export * from "./structure/navervideo"
+export * from "./structure/profile"
+export { Article, Cafe, Comment, NaverVideo, Profile }
+
+import NcAPIStatus from "./talk/ncapistatus"
+import NcBaseChannel from "./talk/ncbasechannel"
+import NcChannel from "./talk/ncbasechannel"
+import NcJoinedChannel from "./talk/ncjoinedchannel"
+import NcJson from "./talk/ncjson"
+import NcMessage from "./talk/ncmessage"
+import uploadImage from "./talk/uploadphoto"
+export * from "./talk/ncapistatus"
+export * from "./talk/ncbasechannel"
+export * from "./talk/ncchannel"
+export * from "./talk/ncjoinedchannel"
+export * from "./talk/ncjson"
+export * from "./talk/ncmessage"
+export * from "./talk/ncprotomsg"
+export * from "./talk/uploadphoto"
+export { NcAPIStatus, NcBaseChannel, NcChannel, NcJoinedChannel,
+    NcJson, NcMessage, uploadImage }
